a211d5848f93cf3bbe7a2be7de824b881ee71278
sunqihua <qihua.sun@qq.com>
2024-04-10 11:30:47 +0800

    remove broken link

--- app/CenteredHero.jsx
+++ app/CenteredHero.jsx
@@ -31,15 +31,15 @@ function CenteredHero() {
   return (
     <>
       <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a
-          href="/hello.com"
+        <Link
+          href="/"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
         >
           <svg className="bi me-2" width={40} height={32}>
             <use xlinkHref="#bootstrap" />
           </svg>
           <span className="fs-4"></span>
-        </a>
+        </Link>
         <ul className="nav nav-pills">
           <li className="nav-item">
             <a href="#" className="nav-link active" aria-current="page">
@@ -79,7 +79,7 @@ function CenteredHero() {
             <input
               class="form-control"
               id="input1"
-              placeholder="Enter a love story title"
+              placeholder="generate a love story title"
               value={note}
               onChange={(e) => setNote(e.target.value)} // Update note state onChange
             />
