0ffbbcfcde489a267ef95de55b3bd086a5768789
sunqihua <qihua.sun@qq.com>
2024-04-11 19:13:58 +0800

    adjust google analyze

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager } from '@next/third-parties/google';
-import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ["latin"] });
 
