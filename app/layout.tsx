94a87764c1bb955b8e4dd57d55fa38b61274eb4c
sunqihua <qihua.sun@qq.com>
2024-04-11 19:13:34 +0800

    adjust google analyze

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,21 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-09WYQ49L6Q"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'G-09WYQ49L6Q');
-                  `,
-        }}
-      />
-      </head>
+       <GoogleTagManager gtmId="G-09WYQ49L6Q" />
       <body className={inter.className}>{children}</body>
     </html>
   );
-
 }
