b6df77e2203d94408a3b3af615cb1591a43eb860
sunqihua <qihua.sun@qq.com>
2024-04-12 14:57:56 +0800

    change site map position

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import CenteredHero from "./CenteredHero"
 import 'bootstrap/dist/css/bootstrap.css';
-import '../styles.css';
+import '/styles.css';
 
 export default function Home() {
     return (
